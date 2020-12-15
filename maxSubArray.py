a35b5e4254bf21c6021397ce0347f2ed6f1eb1c1
hgddjc <71710305+hgddjc@users.noreply.github.com>
2020-12-15 20:57:00 +0800

    Create maxSubArray.py

---
+++ maxSubArray.py
@@ -0,0 +1,8 @@
+#最大子序和
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        size = len(nums)
+        f = [0]*size
+        for i in range(size):
+            f[i] = max(f[i-1]+nums[i],nums[i]) if i else nums[0]
+        return max(f)
